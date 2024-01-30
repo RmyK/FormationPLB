0017b8a128eb74a0d2b28b0ab0b8d566df242678
Rémy <remy.kreder@devconception.fr>
2024-01-30 16:42:36 +0100

    up : ajout blazor

---
+++ EcoleServer/Pages/ListEleveDisplay.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MonEcole.BL;
+using MonEcole.DAL.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoleServer.Pages
+{
+    public partial class ListEleveDisplay : ComponentBase
+    {
+        [Inject]
+        private EleveService eleveService { get; set; }
+
+        public List<Eleve> MesEleves { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEleveAsync();
+            return base.OnInitializedAsync();
+        }
+
+        public void LoadEleveAsync()
+        {
+            MesEleves = eleveService.GetAll();
+        }
+    }
+}
